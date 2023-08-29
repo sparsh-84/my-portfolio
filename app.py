3bdbd4a1b5369140382d6e3702269ad23f209e5f
sparsh-84 <143444194+sparsh-84@users.noreply.github.com>
2023-08-29 20:12:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from typing_extensions import Literal
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
-resume_file = current_dir / "assets" / "CV.pdf"
+# resume_file = current_dir / "assets" / "CV.pdf"
 profile_pic = current_dir / "assets" / "profile-pic.png"
 
 PAGE_TITLE = "Portfolio | Sparsh Goel"
